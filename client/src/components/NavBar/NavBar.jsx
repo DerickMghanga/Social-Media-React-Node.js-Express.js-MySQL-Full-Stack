57685d5d3b04972c30a73ac0b39334cf6ef1752a
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-16 02:54:55 +0300

    navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,28 @@
 import "./navBar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <div className='navBar'>NavBar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to="/" style={{textDecoration: "none"}}>
+          <span>deroo social</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 
