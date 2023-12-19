806265a165d11c752b9483cd0aa42297f5aef86c
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-19 04:56:44 +0300

    Post Item done

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,66 @@
+import "./Post.scss"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import TextsmsIcon from '@mui/icons-material/Textsms';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Link } from 'react-router-dom'
+import { useState } from "react";
+
+const Post = ({ post }) => {
+
+    //TEMPORARY FXN
+    const [liked, setLiked] = useState(false);
+
+    const toggle = () => {
+        setLiked(!liked);
+    }
+
+    return (
+        <div className="post">
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link to={`/profile/${post.userId}`}
+                                style={{ textDecoration: "none", color: "inherit" }}>
+                                <span>{post.name}</span>
+                            </Link>
+                            <span className="date">1 min ago</span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon />
+                </div>
+
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={post?.img} alt="" />
+                </div>
+
+                <div className="info">
+                    <div className="item" onClick={()=>toggle()}>
+                        {
+                            liked ? <FavoriteIcon /> : <FavoriteBorderIcon/>
+                        }
+                        112 likes
+                    </div>
+
+                    <div className="item">
+                        <TextsmsIcon />
+                        12 comments
+                    </div>
+
+                    <div className="item">
+                        <ShareIcon />
+                        Share
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default Post
