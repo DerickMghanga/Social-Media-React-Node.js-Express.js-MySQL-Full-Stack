2fbcd1c29987dc68cad383369d553ab132405bf1
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-20 20:13:37 +0300

    routes and controllers added

---
+++ api/controllers/user.js
@@ -0,0 +1,5 @@
+export const getUser = (req, res) => {
+    //TODO 
+    console.log("request recieved");
+    res.send("Its working fine and well!");
+}
--- api/server.js
+++ api/server.js
@@ -1,8 +1,21 @@
 import Express  from "express";
 const app = Express();
-
 const PORT = 8800;
 
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import postRoutes from "./routes/posts.js";
+
+
+
+// ROUTES
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/likes', likeRoutes);
 
 
 app.listen(PORT, ()=> {
